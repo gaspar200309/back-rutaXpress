de9adece00c7439a9a65d24e82a884f6aceb8070
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2024-09-28 18:44:01 -0400

    first configuration of proyect

---
+++ microservice-user/src/main/java/com/microservice_user/persistence/UserRepository.java
@@ -0,0 +1,11 @@
+package com.microservice_user.persistence;
+
+import com.microservice_user.entities.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByEmail(String email);
+}
---
+++ microservice-user/src/test/java/com/microservice_user/MicroserviceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
