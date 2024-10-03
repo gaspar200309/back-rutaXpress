0fcd783cea264b2325b0a5f93c5ff8e95bb8c156
alfredoT7 <123456alfredotorri@gmail.com>
2024-10-02 21:50:06 -0400

    fix:Gateway paths and port config solved

--- microservice-user/src/main/java/com/microservice_user/config/SecurityConfig.java
+++ microservice-user/src/main/java/com/microservice_user/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,6 +25,10 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .oauth2Client(Customizer.withDefaults())
                 .build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- microservice-user/src/main/java/com/microservice_user/persistence/UserRepository.java
+++ microservice-user/src/main/java/com/microservice_user/persistence/UserRepository.java
@@ -1,11 +1,12 @@
 package com.microservice_user.persistence;
 
 import com.microservice_user.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long>, JpaRepository<User, Long> {
 
     List<User> findByEmail(String email);
 }
