fe146870642dbd98e91fc67dba657ae61aa6c6ef
alfredoT7 <123456alfredotorri@gmail.com>
2024-10-04 19:42:17 -0400

    feat: Register and login backend endpoints finished

---
+++ microservice-user/src/main/java/com/microservice_user/service/AuthService.java
@@ -0,0 +1,83 @@
+package com.microservice_user.service;
+
+import com.microservice_user.dto.ReqRes;
+import com.microservice_user.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import com.microservice_user.repository.OurUserRepo;
+@Service
+public class AuthService {
+
+    @Autowired
+    private OurUserRepo ourUserRepo;
+    @Autowired
+    private JWTUtils jwtUtils;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public ReqRes signUp(ReqRes registrationRequest){
+        ReqRes resp = new ReqRes();
+        try {
+            UserEntity userEntity = new UserEntity();
+            userEntity.setEmail(registrationRequest.getEmail());
+            userEntity.setPassword(passwordEncoder.encode(registrationRequest.getPassword()));
+            userEntity.setRole(registrationRequest.getRole());
+            userEntity.setName(registrationRequest.getName());
+            userEntity.setLastName(registrationRequest.getLastName());
+            userEntity.setPhone(registrationRequest.getPhone());
+            UserEntity ourUserResult = ourUserRepo.save(userEntity);
+            if (ourUserResult != null && ourUserResult.getId() > 0) {
+                resp.setUserEntity(ourUserResult);
+                resp.setMessage("User Saved Successfully");
+                resp.setStatusCode(200);
+            }
+        } catch (Exception e) {
+            resp.setStatusCode(500);
+            resp.setError(e.getMessage());
+        }
+        return resp;
+    }
+
+    public ReqRes signIn(ReqRes signinRequest){
+        ReqRes response = new ReqRes();
+
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(signinRequest.getEmail(),signinRequest.getPassword()));
+            var user = ourUserRepo.findByEmail(signinRequest.getEmail()).orElseThrow();
+            System.out.println("USER IS: "+ user);
+            var jwt = jwtUtils.generateToken(user);
+            var refreshToken = jwtUtils.generateRefreshToken(new HashMap<>(), user);
+            response.setStatusCode(200);
+            response.setToken(jwt);
+            response.setRefreshToken(refreshToken);
+            response.setExpirationTime("24Hr");
+            response.setMessage("Successfully Signed In");
+        }catch (Exception e){
+            response.setStatusCode(500);
+            response.setError(e.getMessage());
+        }
+        return response;
+    }
+
+    public ReqRes refreshToken(ReqRes refreshTokenReqiest){
+        ReqRes response = new ReqRes();
+        String ourEmail = jwtUtils.extractUsername(refreshTokenReqiest.getToken());
+        UserEntity users = ourUserRepo.findByEmail(ourEmail).orElseThrow();
+        if (jwtUtils.isTokenValid(refreshTokenReqiest.getToken(), users)) {
+            var jwt = jwtUtils.generateToken(users);
+            response.setStatusCode(200);
+            response.setToken(jwt);
+            response.setRefreshToken(refreshTokenReqiest.getToken());
+            response.setExpirationTime("24Hr");
+            response.setMessage("Successfully Refreshed Token");
+        }
+        response.setStatusCode(500);
+        return response;
+    }
+}
---
+++ microservice-user/src/main/java/com/microservice_user/service/OurUserDetailsService.java
@@ -0,0 +1,19 @@
+package com.microservice_user.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.microservice_user.repository.OurUserRepo;
+
+@Service
+public class OurUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private OurUserRepo ourUserRepo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return ourUserRepo.findByEmail(username).orElseThrow();
+    }
+}
