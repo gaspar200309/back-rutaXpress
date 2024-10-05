f35cfa82096b181d8657bcb50006278b17ebbdcf
alfredoT7 <123456alfredotorri@gmail.com>
2024-10-04 20:02:09 -0400

    feat: JSON para prueba añadido

--- microservice-user/src/main/java/com/microservice_user/controller/AuthController.java
+++ microservice-user/src/main/java/com/microservice_user/controller/AuthController.java
@@ -16,10 +16,35 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+
+    /**
+     * @param signUpRequest
+     * {
+     *     "email": "123123@com.com",
+     *     "password": "notebok456",
+     *      "role": "DRIVER",
+     *      "name": "Alfredo",
+     *     "lastName": "Torrico",
+     *     "phone": 1234567890
+     * }
+     *
+     * @return 200  OK
+     */
     @PostMapping("/signup")
     public ResponseEntity<ReqRes> signUp(@RequestBody ReqRes signUpRequest){
         return ResponseEntity.ok(authService.signUp(signUpRequest));
     }
+
+
+    /**
+     *
+     * @param signInRequest
+     * {
+     *     "email": "123123@com.com",
+     *     "password": "notebok456"
+     * }
+     * @return 200 ok autorizado
+     */
     @PostMapping("/signin")
     public ResponseEntity<ReqRes> signIn(@RequestBody ReqRes signInRequest){
         return ResponseEntity.ok(authService.signIn(signInRequest));
